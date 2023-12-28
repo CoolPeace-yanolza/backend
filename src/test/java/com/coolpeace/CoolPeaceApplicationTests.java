0a3ef555ec6877be10ffead834ffee54e8877bb8
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-12-28 18:52:37 +0900

    [chore] 프로젝트 초기 세팅

---
+++ src/test/java/com/coolpeace/CoolPeaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coolpeace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolPeaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
