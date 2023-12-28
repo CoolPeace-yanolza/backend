7faa95cbc9b47f939e4a242113152faa42ca7cdb
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-12-28 18:52:24 +0900

    [chore] 프로젝트 초기 세팅

---
+++ src/main/java/com/coolpeace/CoolPeaceApplication.java
@@ -0,0 +1,13 @@
+package com.coolpeace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolPeaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoolPeaceApplication.class, args);
+    }
+
+}
