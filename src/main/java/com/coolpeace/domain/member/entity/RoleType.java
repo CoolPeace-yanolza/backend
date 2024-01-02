cfdfa0f1919b7797e1cf67a5ac2e79160b690db8
rightpair <tkddn204@gmail.com>
2024-01-02 11:28:45 +0900

    [feature] Role 엔티티 및 타입 추가
    설명:
    - #9 를 참고하세요.

---
+++ src/main/java/com/coolpeace/domain/member/entity/RoleType.java
@@ -0,0 +1,14 @@
+package com.coolpeace.domain.member.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum RoleType {
+    USER("ROLE_USER"),
+    OWNER("ROLE_OWNER"),
+    ADMIN("ROLE_ADMIN");
+
+    private final String value;
+}
