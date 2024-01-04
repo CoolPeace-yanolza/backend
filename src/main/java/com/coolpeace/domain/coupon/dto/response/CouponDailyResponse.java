aa8bec9a61ceecd0ea88b189089b988606477810
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2024-01-04 15:12:09 +0900

    [refactor] 빌더 패턴 -> 메소드 방식으로 변경

--- src/main/java/com/coolpeace/domain/coupon/dto/response/CouponDailyResponse.java
+++ src/main/java/com/coolpeace/domain/coupon/dto/response/CouponDailyResponse.java
@@ -1,11 +1,19 @@
 package com.coolpeace.domain.coupon.dto.response;
 
+import com.coolpeace.domain.coupon.entity.CouponDailyCondition;
+import java.util.Collections;
 import java.util.List;
 import lombok.Builder;
 
 @Builder
 public record CouponDailyResponse(
-    String condition,
-    List<String> couponTitle
+    CouponDailyCondition condition,
+    List<String> couponTitles
 ) {
+    public static CouponDailyResponse from(CouponDailyCondition condition,List<String> couponTitles) {
+        return new CouponDailyResponse(condition, couponTitles);
+    }
+    public static CouponDailyResponse from(CouponDailyCondition condition) {
+        return new CouponDailyResponse(condition, Collections.emptyList());
+    }
 }
