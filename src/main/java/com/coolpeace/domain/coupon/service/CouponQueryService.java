53e30939afd6cdbd37370a39c28dc85d875f0809
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2024-01-04 15:12:45 +0900

    [refactor] 동일한 코드 제외 및 메소드 적용

--- src/main/java/com/coolpeace/domain/coupon/service/CouponQueryService.java
+++ src/main/java/com/coolpeace/domain/coupon/service/CouponQueryService.java
@@ -3,6 +3,7 @@ package com.coolpeace.domain.coupon.service;
 import com.coolpeace.domain.coupon.dto.request.CouponDailyRequest;
 import com.coolpeace.domain.coupon.dto.response.CouponDailyResponse;
 import com.coolpeace.domain.coupon.entity.Coupon;
+import com.coolpeace.domain.coupon.entity.CouponDailyCondition;
 import com.coolpeace.domain.coupon.repository.CouponRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -18,24 +19,24 @@ public class CouponQueryService {
 
     /* 메소드 복잡도를 낮추는 방법 고안 중 */
     public CouponDailyResponse dailyReport(CouponDailyRequest request) {
-        if (couponRepository.NoRegister(request.memberId(), request.accommodationId())) {
-            return CouponDailyResponse.builder().condition("등록된 쿠폰이 없음").build();
+        Long memberId = request.memberId();
+        Long accommodationId = request.accommodationId();
+
+        if (couponRepository.noRegister(memberId, accommodationId)) {
+            return CouponDailyResponse.from(CouponDailyCondition.NO_REGISTER);
         }
-        if (couponRepository.NoExposure(request.memberId(), request.accommodationId())) {
-            return CouponDailyResponse.builder().condition("노출 중인 쿠폰이 없음").build();
+        if (couponRepository.noExposure(memberId, accommodationId)) {
+            return CouponDailyResponse.from(CouponDailyCondition.NO_EXPOSURE);
         }
 
-        List<Coupon> expiration3daysCoupons = couponRepository.expiration3days(request.memberId(),
-            request.accommodationId());
+        List<Coupon> expiration3daysCoupons = couponRepository.expiration3days(memberId, accommodationId);
         if (!expiration3daysCoupons.isEmpty()) {
-            return CouponDailyResponse.builder().condition("곧 만료되는 쿠폰이 있음")
-                .couponTitle(expiration3daysCoupons.stream().map(Coupon::getTitle).toList())
-                .build();
+            return CouponDailyResponse.from(CouponDailyCondition.EXPIRATION_3DAYS,
+                expiration3daysCoupons.stream().map(Coupon::getTitle).toList());
         }
-        List<Coupon> exposureCoupons = couponRepository.exposureCoupons(request.memberId(),
-            request.accommodationId());
-        return CouponDailyResponse.builder().condition("아무 조건에 해당하지 않음")
-            .couponTitle(exposureCoupons.stream().map(Coupon::getTitle).toList())
-            .build();
+
+        List<Coupon> exposureCoupons = couponRepository.exposureCoupons(memberId, accommodationId);
+        return CouponDailyResponse.from(CouponDailyCondition.NO_CONDITION,
+            exposureCoupons.stream().map(Coupon::getTitle).toList());
     }
 }
