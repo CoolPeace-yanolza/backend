4e2dc7cc30875b9ba19edc42f984c8eefb7164f8
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2024-01-05 01:43:25 +0900

    [feat] 의존 관계 추가, 통계 계산을 위한 메소드 추가

--- src/main/java/com/coolpeace/domain/statistics/entity/DailyStatistics.java
+++ src/main/java/com/coolpeace/domain/statistics/entity/DailyStatistics.java
@@ -1,9 +1,13 @@
 package com.coolpeace.domain.statistics.entity;
 
+import com.coolpeace.domain.accommodation.entity.Accommodation;
+import com.coolpeace.domain.member.entity.Member;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -18,7 +22,7 @@ public class DailyStatistics {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Size(min=1, max = 31)
+    @Size(min = 1, max = 31)
     private int statisticsDay;
 
     private int totalSales;
@@ -31,4 +35,31 @@ public class DailyStatistics {
 
     private int settlementAmount;
 
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "accommodation_id")
+    private Accommodation accommodation;
+
+    public DailyStatistics(int statisticsDay, Member member, Accommodation accommodation) {
+        this.statisticsDay = statisticsDay;
+        this.member = member;
+        this.accommodation = accommodation;
+    }
+
+    public void setSales(int totalSales, int couponTotalSales) {
+        this.totalSales += totalSales;
+        this.couponTotalSales += couponTotalSales;
+    }
+
+    public void setCoupon(int downloadCount, int usedCount) {
+        this.downloadCount += downloadCount;
+        this.usedCount += usedCount;
+    }
+
+    public void setSettlement(int settlementAmount) {
+        this.settlementAmount += settlementAmount;
+    }
 }
