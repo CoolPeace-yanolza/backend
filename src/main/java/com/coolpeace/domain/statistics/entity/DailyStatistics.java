bbff965f5c0167f8f35acf519d3edba4d5c8fe37
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2024-01-04 19:04:45 +0900

    [feat] 일별 통계 엔티티 추가

---
+++ src/main/java/com/coolpeace/domain/statistics/entity/DailyStatistics.java
@@ -0,0 +1,34 @@
+package com.coolpeace.domain.statistics.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DailyStatistics {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(min=1, max = 31)
+    private int statisticsDay;
+
+    private int totalSales;
+
+    private int couponTotalSales;
+
+    private int downloadCount;
+
+    private int usedCount;
+
+    private int settlementAmount;
+
+}
