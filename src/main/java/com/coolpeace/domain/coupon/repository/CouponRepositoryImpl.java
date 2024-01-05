53cccd7d920e35004bfd6288f4fffe7ae4257762
rightpair <tkddn204@gmail.com>
2024-01-05 20:47:35 +0900

    [feature] 쿠폰 레포지토리에 findAllCoupons 메소드 추가
    설명:
    - 쿠폰 검색을 위한 메소드를 추가했습니다.

--- src/main/java/com/coolpeace/domain/coupon/repository/CouponRepositoryImpl.java
+++ src/main/java/com/coolpeace/domain/coupon/repository/CouponRepositoryImpl.java
@@ -1,19 +1,76 @@
 package com.coolpeace.domain.coupon.repository;
 
+import com.coolpeace.domain.coupon.dto.request.SearchCouponParams;
 import com.coolpeace.domain.coupon.entity.Coupon;
 import com.coolpeace.domain.coupon.entity.type.CouponStatusType;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.data.support.PageableExecutionUtils;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 import static com.coolpeace.domain.coupon.entity.QCoupon.coupon;
 
-@RequiredArgsConstructor
-public class CouponRepositoryImpl implements CouponRepositoryCustom {
+public class CouponRepositoryImpl extends QuerydslRepositorySupport implements CouponRepositoryCustom {
 
     private final JPAQueryFactory jpaQueryFactory;
+
+    public CouponRepositoryImpl(JPAQueryFactory jpaQueryFactory) {
+        super(Coupon.class);
+        this.jpaQueryFactory = jpaQueryFactory;
+    }
+
+    @Override
+    public Page<Coupon> findAllCoupons(Long memberId, SearchCouponParams params, PageRequest pageable) {
+        // 검색 필터링
+        BooleanBuilder searchCouponPredicate = new BooleanBuilder(coupon.member.id.eq(memberId));
+
+        // 쿠폰 상태
+        if (params.status() != null) {
+            searchCouponPredicate.and(switch (params.status()) {
+                case EXPOSURE_ON -> coupon.couponStatus.eq(CouponStatusType.EXPOSURE_ON);
+                case EXPOSURE_OFF -> coupon.couponStatus.eq(CouponStatusType.EXPOSURE_OFF);
+                case EXPIRED -> coupon.couponStatus.eq(CouponStatusType.EXPOSURE_END)
+                        .or(coupon.couponStatus.eq(CouponStatusType.DELETED));
+                case All -> coupon.couponStatus.isNotNull();
+            });
+        }
+
+        // 쿠폰 이름
+        if (params.title() != null) {
+            searchCouponPredicate.and(coupon.title.like(params.title()));
+        }
+
+        // 쿠폰 날짜
+        if (params.date() != null) {
+            searchCouponPredicate.and(switch (params.date()) {
+                case THREE_MONTHS -> coupon.createdAt.after(LocalDateTime.now().minusMonths(3));
+                case SIX_MONTHS -> coupon.createdAt.after(LocalDateTime.now().minusMonths(6));
+                case YEAR -> coupon.createdAt.after(LocalDateTime.now().minusYears(1));
+            });
+        }
+
+        JPAQuery<Coupon> couponJPAQuery = jpaQueryFactory.selectFrom(coupon)
+                .leftJoin(coupon.accommodation)
+                .leftJoin(coupon.room)
+                .where(searchCouponPredicate);
+
+        JPAQuery<Long> totalQuery = jpaQueryFactory.select(coupon.count())
+                .from(coupon)
+                .leftJoin(coupon.accommodation)
+                .leftJoin(coupon.room)
+                .where(searchCouponPredicate);
+
+        List<Coupon> coupons = Objects.requireNonNull(getQuerydsl()).applyPagination(pageable, couponJPAQuery).fetch();
+        return PageableExecutionUtils.getPage(coupons, pageable, totalQuery::fetchOne);
+    }
 
     @Override
     public List<Coupon> exposureCoupons(Long memberId, Long accommodationId) {
