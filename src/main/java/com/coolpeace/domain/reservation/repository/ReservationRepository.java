43041ad3ba6f040dc3457fbae6275eb119304009
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2024-01-05 14:08:56 +0900

    [feat] 해당 메소드 쿼리 레포에서 진행

--- src/main/java/com/coolpeace/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/coolpeace/domain/reservation/repository/ReservationRepository.java
@@ -5,8 +5,6 @@ import com.coolpeace.domain.reservation.entity.Reservation;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReservationRepository extends JpaRepository<Reservation,Long> {
-
-    List<Reservation> findAllByAccommodation(Accommodation accommodation);
+public interface ReservationRepository extends JpaRepository<Reservation,Long>, ReservationRepositoryCustom {
 
 }
