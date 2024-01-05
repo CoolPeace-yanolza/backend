c77d9532282c9ebdae51376524961beca0efa148
rightpair <tkddn204@gmail.com>
2024-01-05 15:12:22 +0900

    [test] 쿠폰 등록 테스트 수정 및 테스트용 데이터 추가

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,7 @@
 INSERT INTO `fc_coolpeace`.role (role_type) VALUES ('ADMIN');
 INSERT INTO `fc_coolpeace`.role (role_type) VALUES ('OWNER');
 INSERT INTO `fc_coolpeace`.role (role_type) VALUES ('USER');
+
+INSERT INTO fc_coolpeace.member (is_deleted, email, name, password) VALUES (0, 'example@email.com', 'Name', 'Password');
+INSERT INTO fc_coolpeace.accommodation (member_id, address, name) VALUES (1, '123 Main St', 'Accommodation 1');
+INSERT INTO fc_coolpeace.room (accommodation_id, room_number, price, room_type) VALUES (1, 101, 50000, 'Single'), (1, 102, 60000, 'Double'), (1, 103, 70000, 'Suite');
