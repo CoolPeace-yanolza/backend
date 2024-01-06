5e251f2fe4e14ff569b93b1aa605af8cdcf9fd3c
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2024-01-06 17:37:54 +0900

    backend-34 쿠폰 대시 보드 월별 업데이트 구현

--- src/main/java/com/coolpeace/domain/statistics/repository/DailyStatisticsRepository.java
+++ src/main/java/com/coolpeace/domain/statistics/repository/DailyStatisticsRepository.java
@@ -2,6 +2,7 @@ package com.coolpeace.domain.statistics.repository;
 
 import com.coolpeace.domain.member.entity.Member;
 import com.coolpeace.domain.statistics.entity.DailyStatistics;
+import java.util.List;
 import java.util.Optional;
 import javax.swing.text.html.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface DailyStatisticsRepository extends JpaRepository<DailyStatistics,Long> {
 
     Optional<DailyStatistics> findByMemberAndStatisticsDay(Member member, int statisticsDay);
+
+    List<DailyStatistics> findAllByMember(Member member);
 
 }
