9bd8eb93b138336a18b0b0f7a6df46352794f0bd
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2024-01-07 17:19:54 +0900

    [feat] 주소로 숙소를 찾는 메소드 구현

--- src/main/java/com/coolpeace/domain/accommodation/repository/AccommodationRepository.java
+++ src/main/java/com/coolpeace/domain/accommodation/repository/AccommodationRepository.java
@@ -3,9 +3,12 @@ package com.coolpeace.domain.accommodation.repository;
 import com.coolpeace.domain.accommodation.entity.Accommodation;
 import com.coolpeace.domain.member.entity.Member;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccommodationRepository extends JpaRepository<Accommodation,Long> {
 
     List<Accommodation> findAllByMember(Member member);
+
+    List<Accommodation> findByAddress(String address);
 }
