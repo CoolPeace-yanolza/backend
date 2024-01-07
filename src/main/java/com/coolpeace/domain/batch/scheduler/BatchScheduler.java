ff898d8ca3abf9de30cdfc002ca8353891303126
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2024-01-07 16:10:49 +0900

    [feat] JobParameters 추가

--- src/main/java/com/coolpeace/domain/batch/scheduler/BatchScheduler.java
+++ src/main/java/com/coolpeace/domain/batch/scheduler/BatchScheduler.java
@@ -1,11 +1,16 @@
 package com.coolpeace.domain.batch.scheduler;
 
-import com.coolpeace.domain.batch.job.StatisticsJob;
+import com.coolpeace.domain.batch.job.DailyStatisticsConfig;
+import com.coolpeace.domain.batch.job.MonthlyStatisticsConfig;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.cglib.core.Local;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -17,22 +22,33 @@ public class BatchScheduler {
 
     private final JobLauncher jobLauncher;
 
-    private final StatisticsJob statisticsJob;
+    private final DailyStatisticsConfig dailyStatisticsConfig;
+
+    private final MonthlyStatisticsConfig monthlyStatisticsConfig;
 
     private final JobRepository jobRepository;
 
     private final PlatformTransactionManager platformTransactionManager;
+
     @Scheduled(cron = "0 0 0 * * *")
     public void runDailyJob() throws Exception {
+        log.info("---------{}---------", LocalDateTime.now());
         log.info("daily scheduler start");
-        jobLauncher.run(statisticsJob.dailyStatisticsJob(jobRepository, platformTransactionManager),
-            new JobParameters());
+        JobParameters jobParameters = new JobParametersBuilder().addString("",
+            LocalDateTime.now().toString()).toJobParameters();
+        jobLauncher.run(
+            dailyStatisticsConfig.dailyStatisticsJob(jobRepository, platformTransactionManager),
+            jobParameters);
     }
     @Scheduled(cron = "0 0 0 11 * *")
     public void runMonthlyJob() throws Exception {
+        log.info("---------{}---------", LocalDateTime.now());
         log.info("monthly scheduler start");
-        jobLauncher.run(statisticsJob.monthlyStatisticsJob(jobRepository, platformTransactionManager),
-            new JobParameters());
+        JobParameters jobParameters = new JobParametersBuilder().addString("",
+            LocalDateTime.now().toString()).toJobParameters();
+        jobLauncher.run(
+            monthlyStatisticsConfig.monthlyStatisticsJob(jobRepository, platformTransactionManager),
+            jobParameters);
     }
 
 
