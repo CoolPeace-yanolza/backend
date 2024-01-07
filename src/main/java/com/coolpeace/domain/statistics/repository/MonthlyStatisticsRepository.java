88aaf5fe8f10d3f9ef277578d9e9ab50f90250c7
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2024-01-07 17:24:20 +0900

    [feat] 숙소로 월간 통계 검색하는 로직 구현

--- src/main/java/com/coolpeace/domain/statistics/repository/MonthlyStatisticsRepository.java
+++ src/main/java/com/coolpeace/domain/statistics/repository/MonthlyStatisticsRepository.java
@@ -1,8 +1,17 @@
 package com.coolpeace.domain.statistics.repository;
 
+import com.coolpeace.domain.accommodation.entity.Accommodation;
 import com.coolpeace.domain.statistics.entity.MonthlyStatistics;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MonthlyStatisticsRepository extends JpaRepository<MonthlyStatistics,Long> {
+public interface MonthlyStatisticsRepository extends JpaRepository<MonthlyStatistics, Long> {
 
+    Optional<MonthlyStatistics> findByAccommodationAndStatisticsYearAndStatisticsMonth(
+        Accommodation accommodation, @Min(value = 2000) int statisticsYear,
+        @Min(value = 1) @Max(value = 12) int statisticsMonth);
+
+    
 }
