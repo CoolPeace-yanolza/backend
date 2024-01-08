9946e677dba6c18b6214250f42bb95996d1757a9
rightpair <tkddn204@gmail.com>
2024-01-08 15:06:31 +0900

    [feature] COUPON_ACCESS_DENIED의 상태 응답을 403으로 변경
    설명:
    - 추가로 사용하지 않는 클래스들을 삭제했습니다.

--- src/main/java/com/coolpeace/domain/coupon/dto/response/CouponRecentHistoryResponse.java
+++
@@ -1,9 +0,0 @@
-package com.coolpeace.domain.coupon.dto.response;
-
-import com.coolpeace.domain.coupon.entity.Coupon;
-
-public record CouponRecentHistoryResponse() {
-    public static CouponRecentHistoryResponse from(Coupon recentHistory) {
-        return new CouponRecentHistoryResponse();
-    }
-}
--- src/main/java/com/coolpeace/domain/coupon/dto/response/CouponSearchResponse.java
+++
@@ -1,9 +0,0 @@
-package com.coolpeace.domain.coupon.dto.response;
-
-import java.util.List;
-
-public record CouponSearchResponse() {
-    public static CouponSearchResponse from(List<CouponResponse> couponResponses) {
-        return new CouponSearchResponse();
-    }
-}
