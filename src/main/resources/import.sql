725b789c5cb9b1e431a69463b77f0a0703b4753b
rightpair <tkddn204@gmail.com>
2024-01-09 17:12:21 +0900

    [chore] import.sql 수정
    설명:
    - 숙박과 객실의 필드가 바뀌어서 쿼리를 삭제했습니다.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,5 +3,3 @@ INSERT INTO `fc_coolpeace`.role (role_type) VALUES ('OWNER');
 INSERT INTO `fc_coolpeace`.role (role_type) VALUES ('USER');
 
 INSERT INTO fc_coolpeace.member (is_deleted, email, name, password) VALUES (0, 'testowner@test.com', 'testowner', 'pass123');
-INSERT INTO fc_coolpeace.accommodation (member_id, address, name) VALUES (1, '123 Main St', 'Accommodation 1');
-INSERT INTO fc_coolpeace.room (accommodation_id, room_number, price, room_type) VALUES (1, 101, 50000, 'Single'), (1, 102, 60000, 'Double'), (1, 103, 70000, 'Suite');
