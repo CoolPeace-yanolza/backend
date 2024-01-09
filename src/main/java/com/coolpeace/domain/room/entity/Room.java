44ccfb932cd328d0ffb503db9cd50e291ef314bd
DESKTOP-TJJRH84\whdgn <whdgns5059@gmail.com>
2024-01-09 11:04:07 +0900

    숙소-객실-쿠폰객실, 예약객실 연관관계

--- src/main/java/com/coolpeace/domain/room/entity/Room.java
+++ src/main/java/com/coolpeace/domain/room/entity/Room.java
@@ -1,13 +1,19 @@
 package com.coolpeace.domain.room.entity;
 
 import com.coolpeace.domain.accommodation.entity.Accommodation;
+import com.coolpeace.domain.coupon.entity.CouponRooms;
 import com.coolpeace.global.common.BaseTimeEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,9 +33,14 @@ public class Room extends BaseTimeEntity {
 
     private int price;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "accommodation_id")
     private Accommodation accommodation;
 
+    @OneToMany(mappedBy = "room", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<RoomReservation> roomReservations = new ArrayList<>();
+
+    @OneToMany(mappedBy = "room", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<CouponRooms> couponRooms = new ArrayList<>();
 
 }
--- src/main/java/com/coolpeace/domain/room/entity/RoomReservation.java
+++ src/main/java/com/coolpeace/domain/room/entity/RoomReservation.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -34,4 +35,5 @@ public class RoomReservation {
     @OneToOne
     @JoinColumn(name = "coupon_id")
     private Coupon coupon;
+
 }
