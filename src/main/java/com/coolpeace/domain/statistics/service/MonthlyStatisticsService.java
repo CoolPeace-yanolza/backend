937b0f8a618d75d8a943e6b4400ceb1ba8520c42
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2024-01-11 01:04:47 +0900

    [feat] 지역 쿠론 다운로드 Top3 로직 구현

--- src/main/java/com/coolpeace/domain/statistics/service/MonthlyStatisticsService.java
+++ src/main/java/com/coolpeace/domain/statistics/service/MonthlyStatisticsService.java
@@ -3,15 +3,14 @@ package com.coolpeace.domain.statistics.service;
 import com.coolpeace.domain.accommodation.entity.Accommodation;
 import com.coolpeace.domain.accommodation.repository.AccommodationRepository;
 import com.coolpeace.domain.coupon.entity.Coupon;
+import com.coolpeace.domain.coupon.exception.CouponNotFoundException;
 import com.coolpeace.domain.coupon.repository.CouponRepository;
 import com.coolpeace.domain.member.entity.Member;
-import com.coolpeace.domain.member.repository.MemberRepository;
 import com.coolpeace.domain.settlement.entity.Settlement;
 import com.coolpeace.domain.settlement.repository.SettlementRepository;
 import com.coolpeace.domain.statistics.entity.DailyStatistics;
 import com.coolpeace.domain.statistics.entity.LocalCouponDownload;
 import com.coolpeace.domain.statistics.entity.MonthlyStatistics;
-import com.coolpeace.domain.statistics.exception.MonthlyStatisticsNotFoundException;
 import com.coolpeace.domain.statistics.repository.DailyStatisticsRepository;
 import com.coolpeace.domain.statistics.repository.LocalCouponDownloadRepository;
 import com.coolpeace.domain.statistics.repository.MonthlyStatisticsRepository;
@@ -27,7 +26,6 @@ import org.springframework.transaction.annotation.Transactional;
 public class MonthlyStatisticsService {
 
     private final DailyStatisticsRepository dailyStatisticsRepository;
-    private final MemberRepository memberRepository;
     private final MonthlyStatisticsRepository monthlyStatisticsRepository;
     private final CouponRepository couponRepository;
     private final LocalCouponDownloadRepository localCouponDownloadRepository;
@@ -45,9 +43,8 @@ public class MonthlyStatisticsService {
                 .findAllByAccommodation(accommodation);
 
             if (!dailyStatisticsList.isEmpty()) {
-                MonthlyStatistics monthlyStatistics = monthlyStatisticsRepository.save(
-                    new MonthlyStatistics(year, month, member,
-                        dailyStatisticsList.get(0).getAccommodation()));
+                MonthlyStatistics monthlyStatistics = monthlyStatisticsRepository
+                    .save(new MonthlyStatistics(year, month, member, accommodation));
 
                 dailyStatisticsList.forEach(dailyStatistics -> monthlyStatistics.setMonthlySum(
                     dailyStatistics.getTotalSales(),
@@ -62,7 +59,7 @@ public class MonthlyStatisticsService {
         });
     }
 
-    public void completeSettlement(){
+    public void completeSettlement() {
         List<Accommodation> accommodations = accommodationRepository.findAll();
 
         accommodations.forEach(accommodation -> {
@@ -75,84 +72,63 @@ public class MonthlyStatisticsService {
         });
     }
 
-    /* 숙소 지역 로직 구현되면, 변경 예정 */
     public void updateCouponDownloadTop3() {
         int month = LocalDate.now().getMonthValue();
         int year = LocalDate.now().getYear();
         List<LocalCouponDownload> localCouponDownloads = localCouponDownloadRepository.findAll();
         localCouponDownloads.forEach(localCouponDownload -> {
-            List<Accommodation> accommodations = accommodationRepository.findByAddress(
+            List<Accommodation> accommodations = accommodationRepository.findAllBySigunguName(
                 localCouponDownload.getRegion());
-            accommodations.forEach(
-                accommodation -> {
-                    monthlyStatisticsRepository
-                    .findByAccommodationAndStatisticsYearAndStatisticsMonth(accommodation, year, month)
-                    .orElseThrow(MonthlyStatisticsNotFoundException::new)
-                    .setLocalCouponDownloadTop3(localCouponDownload);});
-
+            accommodations.forEach(accommodation -> {
+                Member member = accommodation.getMember();
+                monthlyStatisticsRepository
+                    .findByAccommodationAndStatisticsYearAndStatisticsMonth(accommodation, year,
+                        month)
+                    .orElse(monthlyStatisticsRepository.save
+                        (new MonthlyStatistics(year, month, member, accommodation)))
+                    .setLocalCouponDownloadTop3(localCouponDownload);
+            });
         });
     }
 
-    /* 숙소 지역 로직 구현되면, 변경 예정 */
     public void updateLocalCouponDownload() {
         localCouponDownloadRepository.deleteAllInBatch();
         List<Coupon> coupons = couponRepository.findAll();
-        coupons.forEach(coupon -> {
-            String address = coupon.getAccommodation().getAddress();
-            LocalCouponDownload localCouponDownload = localCouponDownloadRepository.findByRegion(
-                address).orElseGet(
-                () -> localCouponDownloadRepository.save(new LocalCouponDownload(address)));
-
-            if (setLocalCouponTitle(coupon, localCouponDownload)) {
-                return;
+        for (Coupon coupon : coupons) {
+            String address = coupon.getAccommodation().getSigungu().getName();
+            LocalCouponDownload localCouponDownload = localCouponDownloadRepository
+                .findByRegion(address)
+                .orElseGet(
+                    () -> localCouponDownloadRepository.save(new LocalCouponDownload(address)));
+            if (localCouponDownload.getFirstCouponTitle().isBlank()) {
+                localCouponDownload.init(coupon.getCouponTitle(), coupon.getId());
+                continue;
             }
             updateLocalCouponTitle(coupon, localCouponDownload);
-        });
-    }
-
-
-    private boolean setLocalCouponTitle(Coupon coupon, LocalCouponDownload localCouponDownload) {
-        boolean isReturn = false;
-      /*  int downloadCount = coupon.getDownLoadCount();
-        if (localCouponDownload.getFirstCouponTitle().isBlank()) {
-            localCouponDownload.setFirstCouponTitle(coupon.getTitle());
-            isReturn = true;
         }
-        if (localCouponDownload.getSecondCouponTitle().isBlank()) {
-            localCouponDownload.setSecondCouponTitle(coupon.getTitle());
-            isReturn = true;
-        }
-        if (localCouponDownload.getThirdCouponTitle().isBlank()) {
-            localCouponDownload.setThirdCouponTitle(coupon.getTitle());
-            isReturn = true;
-        }*/
-        return isReturn;
     }
 
     private void updateLocalCouponTitle(Coupon coupon, LocalCouponDownload localCouponDownload) {
-        /*int downloadCount = coupon.getDownLoadCount();
-        int downloadCount1st = getDownloadCountFromTitle(localCouponDownload.getFirstCouponTitle());
-        int downloadCount2nd = getDownloadCountFromTitle(localCouponDownload.getSecondCouponTitle());
-        int downloadCount3rd = getDownloadCountFromTitle(localCouponDownload.getThirdCouponTitle());
+        int downloadCount = coupon.getDownloadCount();
+        int downloadCount1st = getDownloadCountFromId(localCouponDownload.getFirstCouponId());
+        int downloadCount2nd = getDownloadCountFromId(localCouponDownload.getSecondCouponId());
+        int downloadCount3rd = getDownloadCountFromId(localCouponDownload.getThirdCouponId());
+        String nowCouponTitle = coupon.getCouponTitle();
+        Long nowCouponId = coupon.getId();
 
         if (downloadCount > downloadCount1st) {
-            String temp1st = localCouponDownload.getFirstCouponTitle();
-            String temp2nd = localCouponDownload.getSecondCouponTitle();
-            localCouponDownload.setFirstCouponTitle(coupon.getTitle());
-            localCouponDownload.setSecondCouponTitle(temp1st);
-            localCouponDownload.setThirdCouponTitle(temp2nd);
+            localCouponDownload.setAllCoupon(nowCouponTitle, nowCouponId, localCouponDownload);
         } else if (downloadCount > downloadCount2nd) {
-            String temp2nd = localCouponDownload.getSecondCouponTitle();
-            localCouponDownload.setSecondCouponTitle(coupon.getTitle());
-            localCouponDownload.setThirdCouponTitle(temp2nd);
+            localCouponDownload.setSecondAndThirdCoupon(nowCouponTitle, nowCouponId,
+                localCouponDownload);
         } else if (downloadCount > downloadCount3rd) {
-            localCouponDownload.setThirdCouponTitle(coupon.getTitle());
-        }*/
+            localCouponDownload.setThirdCoupon(nowCouponTitle, nowCouponId);
+        }
+
     }
 
-   /* private int getDownloadCountFromTitle(String title) {
-        return couponRepository.findByTitle(title)
-            .orElseThrow(CouponException::new)
-            .getDownLoadCount();
-    }*/
+    private int getDownloadCountFromId(Long couponId) {
+        return couponRepository.findById(couponId)
+            .orElseThrow(CouponNotFoundException::new).getDownloadCount();
+    }
 }
