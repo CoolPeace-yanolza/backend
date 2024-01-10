50cfdc3ea6e794cc771ba721b95ea28bc877fac8
rightpair <tkddn204@gmail.com>
2024-01-10 18:55:11 +0900

    [feature] 검색 파라미터에 유효성 검사 추가

--- src/main/java/com/coolpeace/domain/coupon/dto/request/SearchCouponParams.java
+++ src/main/java/com/coolpeace/domain/coupon/dto/request/SearchCouponParams.java
@@ -2,10 +2,13 @@ package com.coolpeace.domain.coupon.dto.request;
 
 import com.coolpeace.domain.coupon.dto.request.type.SearchCouponDateFilterType;
 import com.coolpeace.domain.coupon.dto.request.type.SearchCouponStatusFilterType;
+import com.coolpeace.global.common.validator.ValidEnum;
 
 public record SearchCouponParams(
+        @ValidEnum(enumClass = SearchCouponStatusFilterType.class, message = "올바른 쿠폰 상태 값을 입력해주세요.")
         SearchCouponStatusFilterType status,
         String title,
+        @ValidEnum(enumClass = SearchCouponDateFilterType.class, message = "올바른 쿠폰 날짜 값을 입력해주세요.")
         SearchCouponDateFilterType date
 ) {
 }
