5980b028409df6ea6986aa50dfc7badd3c86c55e
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2024-01-11 01:42:40 +0900

    [fix] member 삭제 코드 변경

--- src/main/java/com/coolpeace/domain/coupon/repository/CouponRepository.java
+++ src/main/java/com/coolpeace/domain/coupon/repository/CouponRepository.java
@@ -2,12 +2,10 @@ package com.coolpeace.domain.coupon.repository;
 
 import com.coolpeace.domain.accommodation.entity.Accommodation;
 import com.coolpeace.domain.coupon.entity.Coupon;
-import com.coolpeace.domain.member.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CouponRepository extends JpaRepository<Coupon,Long>,CouponRepositoryCustom {
     boolean existsByMemberIdAndCouponNumber(Long memberId, String couponNumber);
-    List<Coupon> findAllByMemberAndAccommodation(Member member, Accommodation accommodation);
+    List<Coupon> findAllByAccommodation(Accommodation accommodation);
 }
