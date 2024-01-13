e820f3b5d92d032f85178d30a6c84af5b00f2986
rightpair <tkddn204@gmail.com>
2024-01-13 23:58:15 +0900

    [feature] countCouponsByCouponStatus 추가

--- src/main/java/com/coolpeace/domain/coupon/repository/CouponRepositoryCustom.java
+++ src/main/java/com/coolpeace/domain/coupon/repository/CouponRepositoryCustom.java
@@ -3,15 +3,19 @@ package com.coolpeace.domain.coupon.repository;
 
 import com.coolpeace.domain.coupon.dto.request.SearchCouponParams;
 import com.coolpeace.domain.coupon.entity.Coupon;
+import com.coolpeace.domain.coupon.entity.type.CouponStatusType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface CouponRepositoryCustom {
 
     Page<Coupon> findAllCoupons(Long memberId, SearchCouponParams params, PageRequest pageable);
+
+    Map<CouponStatusType, Long> countCouponsByCouponStatus();
 
     List<Coupon> findRecentCouponByMemberId(Long memberId);
 
