def344c2d748b3b6840e9ecbe5b6bfb4f3fe8e36
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2024-01-18 20:40:25 +0900

    [feat] 이미 지난 통계 데이터 삽입을 위한 API 구현

--- src/main/java/com/coolpeace/domain/statistics/controller/StatisticsController.java
+++ src/main/java/com/coolpeace/domain/statistics/controller/StatisticsController.java
@@ -20,27 +20,50 @@ public class StatisticsController {
     private final MonthlyStatisticsService monthlyStatisticsService;
 
     @GetMapping("/daily")
-    public ResponseEntity<?> dailyStatisticsSetting(@RequestParam(defaultValue = "0")int day) {
+    public ResponseEntity<?> dailyStatisticsSetting(
+        @RequestParam(defaultValue = "0") int year,
+        @RequestParam(defaultValue = "0") int month,
+        @RequestParam(defaultValue = "0")int day) {
 
-        dailyStatisticsService.updateSales(day);
-        dailyStatisticsService.updateCoupon(day);
-        dailyStatisticsService.updateSettlement(day);
+            dailyStatisticsService.updateSales(year,month,day);
+            dailyStatisticsService.updateCoupon(year,month,day);
+            dailyStatisticsService.updateSettlement(year,month,day);
+
 
         return ResponseEntity.created(URI.create("/")).build();
     }
 
     @GetMapping("/monthly")
-    public ResponseEntity<?> monthlyStatisticsSetting
-        (@RequestParam(defaultValue = "0") int year,
-            @RequestParam(defaultValue = "0") int month) {
+    public ResponseEntity<?> monthlyStatisticsSetting(
+        @RequestParam(defaultValue = "0") int year,
+        @RequestParam(defaultValue = "0") int month) {
 
-        monthlyStatisticsService.completeSettlement();
-        monthlyStatisticsService.updateMonthlySum(year,month);
-        monthlyStatisticsService.updateLocalCouponDownload();
-        monthlyStatisticsService.updateCouponDownloadTop3(year,month);
+            monthlyStatisticsService.updateMonthlySum(year,month);
+            monthlyStatisticsService.updateLocalCouponDownload(year,month);
+            monthlyStatisticsService.updateCouponDownloadTop3(year,month);
+            monthlyStatisticsService.updateLocalCouponAvg(year,month);
 
         return ResponseEntity.created(URI.create("/")).build();
     }
 
+    @GetMapping("/daily/all")
+    public ResponseEntity<?> dailyStatisticsSettingAll(
+        @RequestParam(defaultValue = "0") int year,
+        @RequestParam(defaultValue = "0") int month) {
+
+        for (int day = 1; day <= calculateDay(month); day++) {
+            dailyStatisticsService.updateSales(year,month,day);
+            dailyStatisticsService.updateCoupon(year,month,day);
+            dailyStatisticsService.updateSettlement(year,month,day);
+        }
+
+        return ResponseEntity.created(URI.create("/")).build();
+    }
+
+    private int calculateDay(int month) {
+        if (month == 2) return 28;
+        if (month == 4 || month == 6 || month == 9 || month == 11) return 30;
+        return 31;
+    }
 
 }
