f4b8ee2904ebc61fc556f49b2235c816f4984230
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2024-01-18 20:36:07 +0900

    [refactor] 단순한 query를 위한 필드 변겨

--- src/main/java/com/coolpeace/domain/settlement/dto/request/SearchSettlementParams.java
+++ src/main/java/com/coolpeace/domain/settlement/dto/request/SearchSettlementParams.java
@@ -3,10 +3,11 @@ package com.coolpeace.domain.settlement.dto.request;
 
 import com.coolpeace.domain.settlement.repository.OrderBy;
 
+
 public record SearchSettlementParams(
-    String startDate,
-    String endDate,
-    OrderBy orderBy
+    String start,
+    String end,
+    OrderBy order
 ){
 
 }
