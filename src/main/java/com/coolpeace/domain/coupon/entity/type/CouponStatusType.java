1ad71f08bc0a7cb05b2eba4b788c2e2b3d3b5498
rightpair <tkddn204@gmail.com>
2024-01-18 19:08:46 +0900

    [feature] 쿠폰 상태 문자열 값 변경
    설명:
    - 쿠폰의 상태를 요구사항에 있는 값과 동일하게 변경했습니다.

--- src/main/java/com/coolpeace/domain/coupon/entity/type/CouponStatusType.java
+++ src/main/java/com/coolpeace/domain/coupon/entity/type/CouponStatusType.java
@@ -6,10 +6,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum CouponStatusType {
-    EXPOSURE_ON("ON"),
-    EXPOSURE_OFF("OFF"),
-    EXPOSURE_WAIT("대기중"),
-    EXPOSURE_END("종료"),
+    EXPOSURE_ON("노출 ON"),
+    EXPOSURE_OFF("노출 OFF"),
+    EXPOSURE_WAIT("노출 대기중"),
+    EXPOSURE_END("노출 기간 만료"),
     DELETED("삭제");
 
     private final String value;
