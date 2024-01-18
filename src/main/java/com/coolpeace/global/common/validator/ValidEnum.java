299f6962f5bdb6f5febd48ddad6ab64851245cbb
rightpair <tkddn204@gmail.com>
2024-01-18 23:34:55 +0900

    [feature] ValuedEnumValidator 수정
    설명:
    - 새로 만든 ValuedEnum의 getValue로 유효성 검증을 하기 위해 ValuedEnumValidator를 수정했습니다.

--- src/main/java/com/coolpeace/global/common/validator/ValidEnum.java
+++ src/main/java/com/coolpeace/global/common/validator/ValidEnum.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Constraint(validatedBy = EnumValidator.class)
+@Constraint(validatedBy = ValuedEnumValidator.class)
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ValidEnum {
