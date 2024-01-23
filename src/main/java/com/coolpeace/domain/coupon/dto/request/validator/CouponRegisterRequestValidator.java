36f963975f5f8ddb997b3d484d46401c8aa127c4
rightpair <tkddn204@gmail.com>
2024-01-23 11:00:36 +0900

    [feature] 쿠폰 등록 및 수정 시 할인 값 관련 수정 및 추가
    설명:
    - DiscountValue 필드를 삭제 후, discountFlatValue, discountFlatRate 로 응답받도록 수정했습니다.
    - 추가 요구사항인 최대 할인 한도 설정 필드를 추가했습니다.
    - 할인 값 관련 유효성 검사 메소드를 추가했습니다.
    - 중복 제거를 위해 추상 클래스를 선언하여 중복되는 메소드를 상위 클래스에 등록하여 사용하도록 수정했습니다.

--- src/main/java/com/coolpeace/domain/coupon/dto/request/validator/CouponRegisterRequestValidator.java
+++ src/main/java/com/coolpeace/domain/coupon/dto/request/validator/CouponRegisterRequestValidator.java
@@ -3,11 +3,9 @@ package com.coolpeace.domain.coupon.dto.request.validator;
 import com.coolpeace.domain.coupon.dto.request.CouponRegisterRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
-import org.springframework.validation.ValidationUtils;
-import org.springframework.validation.Validator;
 
 @Component
-public class CouponRegisterRequestValidator implements Validator {
+public class CouponRegisterRequestValidator extends CouponRequestValidator {
     @Override
     public boolean supports(Class<?> clazz) {
         return CouponRegisterRequest.class.equals(clazz);
@@ -17,12 +15,7 @@ public class CouponRegisterRequestValidator implements Validator {
     public void validate(Object target, Errors errors) {
         CouponRegisterRequest request = (CouponRegisterRequest) target;
 
-        // 방 등록
-        if (!request.registerAllRoom()) {
-            ValidationUtils.rejectIfEmpty(errors,
-                    "registerRooms",
-                    "registerRooms.empty",
-                    "등록할 객실을 선택해주세요.");
-        }
+        validateRegisterRooms(errors, request.registerAllRoom());
+        validateDiscountValues(errors, request.discountType(), request.discountFlatValue(), request.discountFlatRate());
     }
 }
